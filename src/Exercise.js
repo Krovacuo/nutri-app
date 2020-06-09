d67a59ef0ee3390bf8016abddf1e4e2d1bdb8bdf
Krovacuo <42478356+Krovacuo@users.noreply.github.com>
2020-06-09 15:41:17 -0500

    Update Exercise.js

--- src/Exercise.js
+++ src/Exercise.js
@@ -3,15 +3,15 @@ export default class Exercise {
         this.exercise = exercise;
     }
 
-    // app_id: a85620e6 app_key: 8c8ce17420367ed56bc28cb84ce66315
+   
 
     async  getResult() {
         const res = await fetch("https://trackapi.nutritionix.com/v2/natural/exercise", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "x-app-id": "a85620e6",
-                "x-app-key": "8c8ce17420367ed56bc28cb84ce66315",
+                "x-app-id": "",
+                "x-app-key": "",
                 "x-remote-user-id": 0
             },
             body: JSON.stringify({query: this.exercise})
